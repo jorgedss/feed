02d6590f2d3d3acfd50e73428ce6bb70e7bd3bf3
José Jorge <jorgesouza621@gmail.com>
2024-04-08 10:49:04 -0300

    Sidebar finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import {Post} from "./Post"
-import { Header } from "./componets/Header"
+import { Header } from "./components/Header"
 
 import "./global.css"
+import styles from "./App.module.css"
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <div>
       <Header/>
-      <Post
-        author = "Jorge"
-        content = "Três ratos cegos"
-      />
-      <Post
-      author = "José"
-      content = "Três gatos famintos"
-      />
-
+     <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+          author = 'Jorge'
+          content = 'GAVv dshdga  hdhvsqv'
+          />
+        </main>
+        </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import {PencilLine} from 'phosphor-react'
+import styles from './Sidebar.module.css'
+
+export function Sidebar(){
+    return(
+        <aside className={styles.sidebar}> 
+            <img 
+            className= {styles.cover} 
+            src="https://images.unsplash.com/photo-1537884944318-390069bb8665?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/121060792?v=4" />
+
+                <strong>José Jorge</strong>
+                <span>Web Developer</span>
+                <footer>
+                    <a href="#">
+                        <PencilLine size={20}/>
+
+                        Editar seu perfil
+                    </a>
+                </footer>
+
+            </div>
+        </aside>
+    );
+}
